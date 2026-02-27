5d3815d660938bc6a7013da7db7bd29a0a14be5a
milan1990 <hamed.moyasseri2012@gmail.com>
2026-02-27 21:32:51 +0330

    v3

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import "./style.css";
+
+const form = document.getElementById("payment-form");
+const modal = document.getElementById("success-modal");
+const doneBtn = document.getElementById("done-btn");
+
+if (form && modal && doneBtn) {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    modal.classList.remove("hidden");
+    modal.classList.add("flex");
+  });
+
+  doneBtn.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    modal.classList.remove("flex");
+    form.reset();
+  });
+}
